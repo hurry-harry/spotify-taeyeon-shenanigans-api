b89f4debeb57f132ca5cb203078c530d7da75ceb
hurry-harry <harold.francisco1412@gmail.com>
2024-01-10 12:04:20 +0800

    Changed and fixed the Spotify login/authentication process

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-const { createHash } = require("crypto");
 const express = require("express");
 const app = express();
 
@@ -20,11 +19,6 @@ app.listen(port, () => {
     console.log(`Server is running on Port ${port}`);
 });
 
-app.get("/test", (req, res) => {
-    res.send("<h1>Login Requested</h1>");
-    console.log("login");
-});
-
 var generateRandomString = function (length) {
     const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const values = crypto.getRandomValues(new Uint8Array(length));
@@ -32,18 +26,10 @@ var generateRandomString = function (length) {
     return values.reduce((acc, x) => acc + possible[x % possible.length], "");
 };
 
-const codeVerifier = generateRandomString(64);
-
-var hashAndEncode = function (toHash) {
-    return Buffer.from(createHash("sha256").update(toHash).digest("hex")).toString("base64");
-};
-
 app.get("/login", (req, res) => {
     const state = generateRandomString(16);
     res.cookie(process.env.STATE_KEY, state);
 
-    console.log('codeVerifier init', codeVerifier);
-    const codeChallenge = hashAndEncode(codeVerifier);
     const scopes = "user-top-read";
 
     res.redirect(process.env.AUTH_URL +
@@ -51,8 +37,6 @@ app.get("/login", (req, res) => {
             response_type: "code",
             client_id: process.env.CLIENT_ID,
             scope: scopes,
-            code_challenge_method: "S256",
-            code_challenge: codeChallenge,
             redirect_uri: process.env.REDIRECT_URI,
             state: state
         }));
@@ -77,32 +61,26 @@ app.get("/callback/", (req, res) => {
         var authOptions = {
             url: "https://accounts.spotify.com/api/token",
             form: {
-                client_id: process.env.CLIENT_ID,
                 grant_type: "authorization_code",
                 code: code,
-                redirect_uri: process.env.REDIRECT_URI,
-                code_verifier: codeVerifier
+                redirect_uri: process.env.REDIRECT_URI
             },
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization:
+                    "Basic " +
+                    new Buffer.from(process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET).toString("base64"),
             },
             json: true
         };
 
-        console.log('codeVerifier', authOptions.form.code_verifier);
-
         request.post(authOptions, function (error, response, body) {
-            console.log('response', response.statusCode);
-            console.log('body', body);
             if (!error && response.statusCode === 200) {
-                console.log('success');
                 var access_token = body.access_token;
                 var refresh_token = body.refresh_token;
 
                 // redirects back to browser frontend
                 res.redirect(process.env.BASE_PATH + "/?authorized=true#" + access_token);
             } else {
-                console.log('fail');
                 res.redirect(
                     process.env.BASE_PATH +
                     "/?" +
